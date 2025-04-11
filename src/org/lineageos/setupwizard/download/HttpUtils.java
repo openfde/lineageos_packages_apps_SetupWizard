a9829ce90b749637a9582ec2ae82bacda62d3857
gongyong <185457686@qq.com>
2025-04-11 13:47:31 +0800

    update the download url

--- src/org/lineageos/setupwizard/download/HttpUtils.java
+++ src/org/lineageos/setupwizard/download/HttpUtils.java
@@ -15,7 +15,7 @@ import okhttp3.Response;
 public class HttpUtils {
     private static final String TAG = "HttpUtils";
     //
-    public static String APP_INFO_URL = "https://gitee.com/openfde/provision/releases/download/1.3.2/apps.json";
+    public static String APP_INFO_URL = "https://gitee.com/openfde/provision/releases/download/14_2.0.1/apps.json";
 
     private static final OkHttpClient client;
 
